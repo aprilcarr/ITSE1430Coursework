1a42a09a486436be5d6ecdf7c215e156e7608758
April Carr <april.carr@my.tccd.edu>
2018-08-22 18:27:25 -0500

    Hello world program - 1st C#

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int x = 10; //10 is an "integer literal" 'literal = nonchanging values' value of int is 10 expression = value statement = command
+            Console.WriteLine("Hello world");//string literal value = "Hello world" 'Console.WriteLine is a statement'
+        }
+    }
+}
