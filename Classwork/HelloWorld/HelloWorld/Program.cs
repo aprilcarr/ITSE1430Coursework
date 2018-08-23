11fadad393e355ca9ba20a63d9fbdc4f53f213ed
April Carr <april.carr@my.tccd.edu>
2018-08-22 19:13:02 -0500

    lesson 1

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,18 @@ namespace HelloWorld
     {
         static void Main( string[] args )
         {
-            int x = 10; //10 is an "integer literal" 'literal = nonchanging values' value of int is 10 expression = value statement = command
+            //string name;
+            //name = "Bob"  *Below is more efficient
+         
+               string name = "Bob"; // lvalue rvalue IS (id = E sub T)
+            //string Name;  not the same as above 
+
+            double payRate = 4.5;
+
+            Console.WriteLine(name);
+
+            double y = 4.65;
+            int x = 10; //10 is an "integer literal" 'literal = nonchanging values 10 is always 10' value of int is 10 expression = value statement = command
             Console.WriteLine("Hello world");//string literal value = "Hello world" 'Console.WriteLine is a statement'
         }
     }
